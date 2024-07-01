710cc9b7a35b21127a2ff2635b72c655ed7e9ed1
Olivier de WITTE <olivier@burniauxconsulting.com>
2024-07-01 15:32:27 +0200

    First Commit

---
+++ project_addons/test_odw/__manifest__.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Test ODW',
+    'version': '1.0',
+    'category': 'Other',
+    'summary': 'Test ODW Module',
+    'description': """This is a test module""",
+    'depends': ['sale', 'purchase'],
+    'data': [
+    ],
+    'installable': True,
+    'license': 'LGPL-3',
+}
+
