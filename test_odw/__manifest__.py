33ff973d48d727129d3603fb2d5bd1484df6980a
Olivier de WITTE <olivier@burniauxconsulting.com>
2024-08-20 21:25:36 +0200

    Bump to v15.0

---
+++ test_odw/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+
+{
+    'name': 'Test ODW',
+    'version': '15.0.0.0.1',
+    'category': 'Other',
+    'summary': 'Test ODW Module',
+    'description': """This is a test module""",
+    'depends': ["base", "sale", "purchase"],
+    'data': [
+        "views/res_partner_views.xml",
+    ],
+    'installable': True,
+    'license': 'LGPL-3',
+}
+
